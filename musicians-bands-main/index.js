db50ee2ea1301877fa8f4703c14be1bb76fa30be
mdbrown543 <95253598+mdbrown543@users.noreply.github.com>
2022-05-13 12:04:32 -0400

    Added many2many association

--- musicians-bands-main/index.js
+++ musicians-bands-main/index.js
@@ -1,13 +1,18 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
+const {Song} = require('./Song')
 
 //one-to-many relationship
 Band.hasMany(Musician)
 Musician.belongsTo(Band,{
     foreignKey: 'bandId'
   });
+//many-to-many relationship
+Band.belongsToMany(Song,{through: "band_songs"});
+Song.belongsToMany(Band,{through: "band_songs"});
 
 module.exports = {
     Band,
-    Musician
+    Musician,
+    Song
 };
--- musicians-bands-main/index.test.js
+++ musicians-bands-main/index.test.js
@@ -1,5 +1,5 @@
 const {sequelize} = require('./db');
-const {Band, Musician} = require('./index')
+const {Band, Musician, Song} = require('./index')
 
 describe('Band and Musician Models', () => {
     /**
@@ -55,8 +55,8 @@ describe('Band and Musician Models', () => {
 		expect(musicians.length).toBe(2)
 		expect(musicians[1] instanceof Musician).toBeTruthy
     })
-    test('can update a Musician instance', async () => {
-        // TODO - test creating a musician
+    test('can add musicians to a band', async () => {
+        
         const phish = await Band.create({name : 'Phish', genre : 'Rock'})
         const spoon = await Band.create({name : 'Spoon', genre : 'Rock'})
 		const bob = await Musician.create({name : 'Bob', instrument : 'guitar'});
@@ -68,6 +68,26 @@ describe('Band and Musician Models', () => {
         
         expect(spoon.getMusicians()).toBeTruthy();
         expect(phish.getMusicians()).toBeTruthy();
+      
+    })
+    test('can add songs to band and vice versa', async () => {
+        
+        const phish = await Band.create({name : 'Phish', genre : 'Rock'})
+        const spoon = await Band.create({name : 'Spoon', genre : 'Rock'})
+		const perfect = await Song.create({title : 'Perfect', year : 1990});
+        const hello = await Song.create({title : 'Hello', year : 1999});
+		
+		await phish.addSong(perfect)
+        await spoon.addSong(hello)
+        const phishBand = await Band.findByPk(1)
+        await phishBand.addSong(1)
+        const helloSong = await Song.findByPk(2)
+        await helloSong.addBand(2)
+        const phishSongs = await phishBand.getSongs()
+        const helloBand = await helloSong.getBands()
+        console.log(helloBand)
+        expect(helloBand[0].name).toBe('Spoon');
+        expect(phishSongs[0].title).toBe('Perfect');
       
     })
 })
